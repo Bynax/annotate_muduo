1e474f08d064f6b0a2d1a7591a90f674f1476f44
bohuanshi <bohuanshi@163.com>
2020-04-05 22:11:29 +0800

    thread undo

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+//#include "base/tests/Timestamp_test.cpp"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+   unordered_map<int,int>a;
+   a[2] = 1;
+   a[3] = 2;
+   a[7] = 5;
+   a.erase(7);
+
 }
+
+
+
